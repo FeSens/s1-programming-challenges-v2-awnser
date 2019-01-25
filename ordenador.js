094c3aef4f64c553052cd28acfbc5db168435a06
fesens <felipesensbonetto@gmail.com>
2019-01-25 14:48:39 -0200

    Add exception OrderingException

--- ordenador.js
+++ ordenador.js
@@ -1,7 +1,15 @@
 var firstBy = require('thenby');
 
+function OrderingException(message) {
+    this.message = message;
+    this.name = "OrderingException";
+};
+
 function ordenar(lista, settings) {
-     
+    
+    if (lista == null){
+        throw new OrderingException("A lista entregue para ordenação não é valida");
+    };
     lista.sort(
         firstBy(settings[0]["propriedade"],settings[0]["ordem"])
         .thenBy(settings[1]["propriedade"],settings[1]["ordem"])
